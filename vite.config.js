b2004154e8f5eadf20d5f43f6f7136aca86704fa
AzizChahlaoui <ingenieursirif@gmail.com>
2026-02-20 16:00:43 +0100

    fix vite base for Vercel

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    base: "/",  // <-- changer ici pour Vercel
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
