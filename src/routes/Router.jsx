23b6a3eab1e0ef5f1a5b261d6c8a498cc8c98409
AzizChahlaoui <ingenieursirif@gmail.com>
2026-02-20 16:11:26 +0100

    fix vite route name

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,13 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Loading from "../components/common/loading/Loading";
+
 const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
 
+// Récupère le nom du repo depuis VITE_REPO_NAME si défini (utile pour GitHub Pages)
 const repoName = import.meta.env.VITE_REPO_NAME || "";
+const baseName = repoName ? `/${repoName}` : "/"; // Si vide, utilise "/"
 
 export const router = createBrowserRouter(
   [
@@ -18,10 +21,10 @@ export const router = createBrowserRouter(
       children: [
         {
           path: "/",
-          element: <Home></Home>,
+          element: <Home />,
         },
       ],
     },
   ],
-  { basename: `/${repoName}` }
+  { basename: baseName } // <-- utiliser le basename corrigé
 );
